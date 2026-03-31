b83a737b3e9abaed820a879a61ee190bc62c5166
Admin <Admin@ADMIN-PC>
2026-03-31 07:11:45 +0700

    all cm

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllocation/Validators/CreateLeaveAllocationDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using HR.LeaveManagement.Application.Persistence;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveAllocation.Validators
+{
+    public class CreateLeaveAllocationDtoValidator : AbstractValidator<CreateLeaveAllocationDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveAllocationDtoValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+
+            RuleFor(p => p.LeaveTypeId)
+                .GreaterThan(0)
+                .MustAsync(async (id, token) =>
+                {
+                    var leaveTypeExists = await _leaveTypeRepository.Exists(id);
+                    return leaveTypeExists;
+                })
+                .WithMessage("{PropertyName} does not exist.");
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveType.Validators
+{
+    public class CreateLeaveTypeDtoValidator : AbstractValidator<CreateLeaveTypeDto>
+    {
+
+        public CreateLeaveTypeDtoValidator()
+        {
+            Include(new ILeaveTypeDtoValidator());
+        }
+    }
+}
